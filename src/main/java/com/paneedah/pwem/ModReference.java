e51aa18d6bd37e66b57a47a97818728359281ff3
Paneedah <paneedah.github@panada.cc>
2023-07-10 01:32:34 +0100

    Removed RedCore dependency.

--- src/main/java/com/paneedah/pwem/ModReference.java
+++ src/main/java/com/paneedah/pwem/ModReference.java
@@ -1,11 +1,7 @@
 package com.paneedah.pwem;
 
-import io.redstudioragnarok.redcore.logging.RedLogger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.net.URI;
-import java.net.URISyntaxException;
 
 public class ModReference {
 
@@ -14,13 +10,4 @@ public class ModReference {
     public static final String VERSION = Tags.VERSION;
     public static final String DEPENDENCIES = "required:mwc@[0.1,)";
     public static final Logger LOG = LogManager.getLogger(NAME);
-    public static final RedLogger REDLOG;
-
-    static {
-        try {
-            REDLOG = new RedLogger(NAME, new URI("https://github.com/Cubed-Development/PWEM/issues"), LOG);
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
-    }
 }
